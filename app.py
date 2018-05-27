6d02ac9e5836680e6b8777b0ccbf4082441f69ae
Rohan Burke <coolq1000@gmail.com>
2018-05-27 12:50:16 +1000

    Initial commit :)

---
+++ app.py
@@ -0,0 +1,29 @@
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, send, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form['name'])
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html')
+
+@socketio.on('client_connected')
+def handle_connect():
+    print('Connected!')
+
+@socketio.on('message')
+def handle_message(message):
+    emit('echo', message, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ static/JS/app.js
@@ -0,0 +1,29 @@
+// <div id="chat-message"><span>Test</span></div>
+
+let socket = io.connect('http://' + document.domain + ':' + location.port);
+
+socket.on('connect', function() {
+    socket.emit('client_connected');
+});
+
+socket.on('echo', function(message) {
+    create_message(message['name'], message['content']);
+});
+
+function send_message(name, content)
+{
+    socket.emit('message', {name: sessionStorage.getItem('name'), content: content});
+}
+
+function create_message(name, content)
+{
+    console.log(name);
+    let parent = document.getElementById('chat-history');
+    parent.insertAdjacentHTML('beforeend', `<div id="chat-message"><span>${name}: ${content}</span></div>`)
+    parent.scrollTo(0, parent.scrollHeight);
+}
+
+if (sessionStorage.getItem('name') == null)
+{
+    window.location = 'login';
+}
